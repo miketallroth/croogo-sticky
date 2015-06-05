024371046fe2167c3125c16e4dfffda05ba5c494
Michael Tallroth <miket@imperium.neenah.na.plexus.com>
2015-06-05 13:52:11 -0500

    cleaned up paginate order addition, accomodating other possible incoming situations.

--- Controller/Component/StickyComponent.php
+++ Controller/Component/StickyComponent.php
@@ -7,17 +7,32 @@ class StickyComponent extends Component {
 
     //called after Controller::beforeFilter()
     public function startup(Controller $controller) {
-        $controller->paginate['Node']['order'] = array(
-            'Node.sticky' => 'DESC',
-        );
-        $controller->paginate['Node']['limit'] = 20;
-        CakeLog::write('debug',print_r($controller->paginate,true));
-        CakeLog::write('debug',$this->action);
-    }
 
-    public function beforeRender(Controller $controller) {
-        CakeLog::write('debug',print_r($controller->paginate,true));
-        CakeLog::write('debug',$this->action);
+        $stickyOrder = array('Node.sticky' => 'DESC');
+
+        if (!array_key_exists('Node', $controller->paginate)) {
+            $controller->paginate = array('Node' => array());
+        }
+        
+        if (!array_key_exists('order', $controller->paginate['Node'])) {
+            $order = $stickyOrder;
+        } else {
+            $order = $controller->paginate['Node']['order'];
+            if (empty($order)) {
+                $order = $stickyOrder;
+            } else {
+                if (is_array($order)) {
+                    array_unshift($order, $stickyOrder);
+                } else {
+                    $oldOrder = $order;
+                    $order = $stickyOrder;
+                    $order[] = $oldOrder;
+                }
+            }
+        }
+
+        $controller->paginate['Node']['order'] = $order;
+        $controller->paginate['Node']['limit'] = 20;
     }
 
 }
